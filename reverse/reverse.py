0e99a20a40e08d150b87b4795e969dff8bebc8ce
Guillermo Alfaro <galfarotolon@gmail.com>
2020-07-17 12:58:03 -0500

    finished reversed list

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -38,5 +38,53 @@ class LinkedList:
 
         return False
 
+
     def reverse_list(self, node, prev):
-        pass
+        
+        #check if list has nodes or if it is empty
+        # This is the way to exit the loop when using recursion
+
+        if self.head is None:
+            return
+        
+         # If there are nodes, then begin the recursion in each node
+
+         #store the current node as the head of the list, as starting point
+        current_node = self.head
+
+
+        # set the previous node to be none for now
+        previous_node = None
+
+
+       # loop through the list as long as there is something in it
+
+        while current_node is not None:
+
+            # make a variable to point the current node to the next one
+
+           nextNode = current_node.next_node
+
+        # the current now becomes the previous node
+           current_node.next_node = previous_node
+
+           # previous becomes current
+
+           previous_node = current_node
+
+           # current becomes next
+
+           current_node = nextNode
+
+           # once exit the loop
+           # set the head to the previous node, which is the last
+           # node in the list before it was reversed
+
+        self.head = previous_node
+
+          
+
+
+
+
+   
